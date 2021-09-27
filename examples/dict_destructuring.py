fa60d8c59b49ea451ce557d7ea2969efc4b463d4
Ben Ferriman <ben.ferriman@rtslabs.com>
2021-09-27 16:17:49 -0600

    added dictionary destructuring example

---
+++ examples/dict_destructuring.py
@@ -0,0 +1,14 @@
+my_dict = {
+    'color': 'red',
+    'animal': 'pony',
+}
+
+key1, key2 = my_dict
+
+print("key1:", key1)
+print("key2:", key2)
+
+color, animal = my_dict.values()
+
+print("color:", color)
+print("animal:", animal)
